a398d92e67e56e53f241987588356d5ace62655b
orifrove <orifsokirhodzaev@gmail.com>
2026-04-23 18:57:06 +0500

    Fix, register error

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from core.views import home
+from apps.users.views import LoginPageView, RegisterPageView, LogoutPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Главная
     path('', home, name='home'),
+    # Auth страницы
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('register/', RegisterPageView.as_view(), name='register'),
+    path('logout/', LogoutPageView.as_view(), name='logout'),
     # API
     path('api/auth/', include('apps.users.urls', namespace='users')),
     path('api/aircraft/', include('apps.aircraft.urls')),
@@ -17,7 +22,6 @@ urlpatterns = [
     path('flights/', include('apps.flights.urls', namespace='flights')),
     path('bookings/', include('apps.bookings.urls', namespace='bookings')),
     path('aircraft/', include('apps.aircraft.urls', namespace='aircraft')),
-    path('login/', include('apps.users.urls', namespace='users_login')),
     # Docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
